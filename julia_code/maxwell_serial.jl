974903f403bbaed6cc25296463a5be5f1049f80b
Pierre Navaro <navaro@math.cnrs.fr>
2025-10-16 22:20:19 +0200

    Juliac (#10)
    * Add Makefile for Julia app build and refactor code
    
    Introduced a Makefile in julia_code to enable building a standalone executable using JuliaC. Refactored fdtd.jl and mesh.jl to add type annotations and improve constructors. Updated maxwell_serial.jl to support command-line arguments, removed unused variables, and improved output handling. Updated README with build instructions and clarified usage details.
    
    * Update maxwell_serial.jl

--- julia_code/maxwell_serial.jl
+++ julia_code/maxwell_serial.jl
@@ -1,21 +1,31 @@
-const c = 1.0 # speed of light
-const csq = c * c
+const csq = 1.0
 
 include("mesh.jl")
 include("fdtd.jl")
 
+function @main(ARGS)
 
-function main(nstep)
+	if length(ARGS) == 0
+		println(Core.stdout, "No command line arguments provided. nstep = 1")
+        nstep = 1
+	else
+        nstep = parse(Int, first(ARGS))
+	end
 
-    @show cfl = 0.1     # Courant-Friedrich-Levy
-    @show tfinal = 10.0     # final time
-    @show nstepmax = 1000  # max steps
-    @show md = 2     # md : wave number x (initial condition)
-    @show nd = 2     # nd : wave number y (initial condition)
-    @show nx = 1200     # x number of points
-    @show ny = 1200     # y number of points
-    @show dimx = 1.0     # width
-    @show dimy = 1.0     # height
+	println(Core.stdout, nstep)
+
+    c = 1.0 # speed of light
+    csq = c * c
+
+    cfl = 0.1        # Courant-Friedrich-Levy
+    tfinal = 10.0    # final time
+    nstepmax = 1000  # max steps
+    md = 2           # md : wave number x (initial condition)
+    nd = 2           # nd : wave number y (initial condition)
+    nx = 1200        # x number of points
+    ny = 1200        # y number of points
+    dimx = 1.0       # width
+    dimy = 1.0       # height
 
     dx = dimx / nx
     dy = dimy / ny
@@ -26,7 +36,7 @@ function main(nstep)
 
     dt = cfl / sqrt(1 / dx^2 + 1 / dy^2) / c
 
-    @show nstep = min(nstepmax, nstep)
+    nstep = min(nstepmax, nstep)
 
     fields = MeshFields(mesh)
 
@@ -43,38 +53,40 @@ function main(nstep)
         )
     end
 
-    tag = 1111
-
+    time = 0.0
+    err_l2 = Inf
 
     for istep in 1:nstep # Loop over time
 
         # Ex(n) [1:nx]*[1:ny+1] --> B(n+1/2) [1:nx]*[1:ny]
         # Ey(n) [1:nx+1]*[1:ny] --> B(n+1/2) [1:nx]*[1:ny]
 
-        ampere_maxwell!(fields, 1, nx, 1, ny, dt)
+        faraday!(fields, 1, nx, 1, ny, dt)
+
+        time += 0.5dt
 
         periodic_bc!(fields, 1, nx, 1, ny, dt)
 
-        faraday!(fields, 1, nx, 1, ny, dt)
+        ampere_maxwell!(fields, 1, nx, 1, ny, dt)
+
 
         err_l2 = 0.0
         time = (istep - 0.5) * dt
         for j in 1:ny, i in 1:nx
             th_bz = (
                 -cos(md * pi * ((i - 0.5) * dx / dimx)) *
-                    cos(nd * pi * ((j - 0.5) * dy / dimy)) *
-                    cos(omega * time)
+                 cos(nd * pi * ((j - 0.5) * dy / dimy)) *
+                 cos(omega * time)
             )
             err_l2 += (fields.bz[i, j] - th_bz)^2
         end
 
-        println(sqrt(err_l2))
+        time += 0.5dt
+
+        println(Core.stdout, sqrt(err_l2))
 
     end # next time step
 
-    return
+
+    return 0
 end
-
-main(1) # trigger building
-
-@time println(main(1000))
